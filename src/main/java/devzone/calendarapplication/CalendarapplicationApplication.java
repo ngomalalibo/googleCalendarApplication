ac1d0f0d1e4b13a628d788746566e248d450aaa8
Ngo Alalibo <ngomalalibo@yahoo.com>
2018-10-03 20:31:50 +0100

    Committing to Github

--- src/main/java/devzone/calendarapplication/CalendarapplicationApplication.java
+++ src/main/java/devzone/calendarapplication/CalendarapplicationApplication.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Configuration
-//@PropertySource(value = {"classpath:application.properties"})
-@EnableOAuth2Sso
 @SpringBootApplication
 public class CalendarapplicationApplication
 {
