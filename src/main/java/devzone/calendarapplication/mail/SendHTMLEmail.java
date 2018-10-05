17bdd7da381dc294bffce9c56c59b46e89f9e986
Ngo Alalibo <ngomalalibo@yahoo.com>
2018-10-05 17:35:18 +0100

    Committing to Github

--- src/main/java/devzone/calendarapplication/mail/SendHTMLEmail.java
+++ src/main/java/devzone/calendarapplication/mail/SendHTMLEmail.java
@@ -35,9 +35,11 @@ public class SendHTMLEmail {
         properties.setProperty("mail.smtp.host", host);
         properties.setProperty("mail.smtp.user", username);
         properties.setProperty("mail.smtp.password", password);
-        //properties.setProperty("mail.smtp.port", d_port);
+        properties.setProperty("mail.smtp.port", d_port);
         properties.setProperty("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
     
         // Get the default Session object.
         //Session session = Session.getDefaultInstance(properties);
@@ -49,6 +51,7 @@ public class SendHTMLEmail {
                 return new PasswordAuthentication(username,password);
             }
         });
+        session.setDebug(true);
     
         try {
             // Create a default MimeMessage object.
@@ -62,12 +65,14 @@ public class SendHTMLEmail {
         
             // Set Subject: header field
             message.setSubject("Login alert. Successful login to calendar App!");
+            
+            message.setText("Login alert: Message! ");
         
             // Send the actual HTML message, as big as you like
             message.setContent("<h1>This is actual message</h1>", "text/html");
         
             // Send message
-            Transport transport = session.getTransport("smtp");
+            Transport transport = session.getTransport("smtps");
             transport.connect(host, Integer.valueOf(d_port), username, password);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
